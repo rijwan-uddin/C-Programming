8909cb6073a33af1e7fa287f86ae820d3729df7e
Md Rijwan Uddin Sarker <96045123+rijwan-uddin@users.noreply.github.com>
2024-02-01 22:47:24 +0600

    Add files via upload

---
+++ 8.20.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    int i,j,n;
+    printf("How many line: ");
+    scanf("%d",&n);
+    for (i=n; i>=1; i--)
+    {
+        for (j=1; j<=i; j++)
+       printf("%4c",j+64);
+       printf("\n") ;
+    }
+}
