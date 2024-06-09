3ede67f5565dfbe466151ac4b385b44dd7a05fef
Md Rijwan Uddin Sarker <96045123+rijwan-uddin@users.noreply.github.com>
2024-06-09 23:08:55 +0600

    Add files via upload

---
+++ 8.32.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+main()
+{
+    int i,j,n;
+    printf("How many line: ");
+    scanf("%d",&n);
+    for (i=1; i<=n; i++)
+{
+
+        for (j=1; j<=n-i; j++)
+         printf("    ");
+
+        for (j=1; j<=i;j++)
+       printf("%4d",j);
+
+       for (j=i-1; j>=1; j--)
+       printf("%4d",j);
+       printf("\n") ;
+    }
+ for (i=n-1; i>=1; i--)
+{
+
+        for (j=1; j<=n-i; j++)
+         printf("    ");
+
+        for (j=1; j<=i;j++)
+       printf("%4d",j);
+
+       for (j=i-1; j>=1; j--)
+       printf("%4d",j);
+       printf("\n") ;
+    }
+
+}
