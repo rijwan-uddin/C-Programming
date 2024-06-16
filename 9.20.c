69d20b3dfee18ee097b377377c37a7bceaf72c69
Md Rijwan Uddin Sarker <96045123+rijwan-uddin@users.noreply.github.com>
2024-06-16 18:23:11 +0600

    Add files via upload

---
+++ 9.20.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+    int n,i,isprime;
+    printf("enter any integer number:");
+    scanf("%d",&n);
+    if (n<2)
+        isprime=0;
+    else
+        {
+            isprime=1;
+            for (i=2;i<n;i++)
+                if (n%i==0)
+                {
+
+                isprime=0;
+                break;
+                }
+        }
+        if (isprime==1)
+           printf("prime");
+        else
+           printf("not prime");
+}
+
