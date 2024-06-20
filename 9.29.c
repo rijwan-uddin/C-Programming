f387d6e6839b1f064cba595f183713031f750603
Md Rijwan Uddin Sarker <96045123+rijwan-uddin@users.noreply.github.com>
2024-06-20 22:46:54 +0600

    Add files via upload

---
+++ 9.29.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+    long a,b,c;
+    int n,i;
+    printf("n=");
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    for (i=1;a<=n;i++)
+    {
+        printf("%10ld",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
